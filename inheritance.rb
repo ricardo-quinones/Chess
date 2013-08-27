a1232653c9efd254cec7bc2d0f68699e71bb2b35
Sarah Mogin <sarah.mogin@gmail.com>
2013-08-27 09:45:52 -0400

    Fixes bonus method

--- inheritance.rb
+++ inheritance.rb
@@ -26,7 +26,7 @@ class Manager < Employee
     @employees = []
   end
 
-  def add_employee(*employee)
+  def add_employees(*employee)
     employee.each do |emp|
       emp.manager = self
       @employees << emp
@@ -37,36 +37,12 @@ class Manager < Employee
     total_salaries = 0
     self.employees.each do |emp|
       total_salaries += emp.salary
-      p emp.class
       total_salaries += emp.sub_salaries if emp.class == Manager
     end
     total_salaries
   end
 
   def bonus(multiplier)
-    sub_employee_sal = 0
-    @employees.each do |emp|
-      sub_employee_sal += emp.salary
-
-    end
-    sub_employee_sal * multiplier
+    self.sub_salaries * multiplier
   end
 end
-
-seth = Manager.new(80000, "Dev slave")
-ricky = Employee.new(85000, "Dev underling")
-sid = Manager.new(100000, "Dev superstar")
-seth.add_employee(ricky)
-sid.add_employee(seth)
-
-# p seth.salary
-# seth.bonus(0.01)
-# sid.employees.each do |emp|
-#   p emp
-# end
-# p sid
-# p seth.bonus(0.01)
-# p ricky.bonus(0.01)
-# p sid.bonus(0.01)
-
-p sid.sub_salaries
